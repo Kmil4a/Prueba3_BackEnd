95969c29bf33ec5e19e2b234de71cc8065db14d4
Kmil4a <camila.araya68@inacapmail.cl>
2024-12-27 01:20:43 -0300

    Prueba lista :DDDDD

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class ToDo(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    # Relación con el usuario
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from .models import ToDo
 from .forms import FormTodo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-
+@login_required 
 def home(request):
     tarea_a_editar = None
     if request.method == 'POST':
         if 'save' in request.POST:
             form = FormTodo(request.POST)
             if form.is_valid():
-                form.save()
+                tarea = form.save(commit=False)
+                tarea.user = request.user
+                tarea.save()
 
         elif 'delete' in request.POST:
             pk = request.POST.get('delete')
-            ToDo.objects.get(pk=pk).delete()
+            ToDo.objects.get(pk=pk, user=request.user).delete()
 
         elif 'toggle' in request.POST:
             pk = request.POST.get('toggle')
@@ -25,7 +29,7 @@ def home(request):
         elif 'edit' in request.POST:
             pk = request.POST.get('edit')
             tarea_a_editar = get_object_or_404(ToDo, pk=pk)
-            
+
         elif 'update' in request.POST:
             pk = request.POST.get('id')
             tarea = get_object_or_404(ToDo, pk=pk)
@@ -35,10 +39,22 @@ def home(request):
 
         return HttpResponseRedirect(request.path_info)
 
-    tareas = ToDo.objects.all()
+    tareas = ToDo.objects.filter(user=request.user)
     form = FormTodo(instance=tarea_a_editar) if tarea_a_editar else FormTodo()
     return render(
         request,
         'home.html',
         {'tareas': tareas, 'form': form, 'tarea_a_editar': tarea_a_editar},
     )
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+           
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
